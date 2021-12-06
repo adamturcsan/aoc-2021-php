1869303fb91f3335990aafb086b3f59338205940
Turcsán Ádám <turcsan.adam@legow.hu>
2021-12-06 13:34:28 +0100

    Keep only the fast and simple algorithm

--- src/daySix/solution.php
+++ src/daySix/solution.php
@@ -1,38 +1,28 @@
 <?php
 
-$input = file_get_contents(__DIR__ . '/input');
+$pool = new FishPool(file_get_contents(__DIR__ . '/input'));
 
-$pool = new FishPool(...parseInput($input));
-
-echo "1st solution: " . $pool->fastSimulation(80) . "\n";
-echo "2nd solution: " . $pool->fastSimulation(256) . "\n";
-
-function parseInput(string $input): array
-{
-    $fishAges = explode(',', $input);
-    return array_map(fn(int $age) => new Fish($age), $fishAges);
-}
+echo "1st solution: " . $pool->simulate(80) . "\n";
+echo "2nd solution: " . $pool->simulate(256) . "\n";
 
 class FishPool
 {
-    private array $fishes;
-    private SplFixedArray $simulationPool;
     private array $fishCountByAge = [];
 
-    public function __construct(Fish ... $fishes)
+    public function __construct(string $input)
     {
-        $this->fishes = $fishes;
+        $ages = array_map(fn(string $age) => intval($age), explode(',', $input));
         $this->fishCountByAge = array_reduce(
-            $fishes,
-            function($carry, $fish) {
-                $carry[$fish->getAge()]++;
+            $ages,
+            function($carry, $age) {
+                $carry[$age]++;
                 return $carry;
             },
             [0,0,0,0,0,0,0,0,0]
         );
     }
 
-    public function fastSimulation(int $forDays): int
+    public function simulate(int $forDays): int
     {
         $simulationCounts = $this->fishCountByAge;
         for ($i = $forDays; $i > 0; $i--) {
@@ -43,53 +33,11 @@ class FishPool
                 $simulationCounts[4],
                 $simulationCounts[5],
                 $simulationCounts[6],
-                $simulationCounts[7]+$simulationCounts[0],
+                $simulationCounts[7] + $simulationCounts[0],
                 $simulationCounts[8],
                 $simulationCounts[0],
             ];
         }
         return array_sum($simulationCounts);
-    }
-    
-    public function simulate(int $forDays): int
-    {
-        $this->simulationPool = SplFixedArray::fromArray($this->fishes);
-        for ($i = $forDays; $i > 0; $i--) {
-            $newBorns = [];
-            foreach ($this->simulationPool as $fish) {
-                $newBornOrNull = $fish->doAge();
-                if ($newBornOrNull !== null) {
-                    $newBorns[] = $newBornOrNull;
-                }
-            }
-            $index = count($this->simulationPool);
-            $this->simulationPool->setSize(count($this->simulationPool) + count($newBorns));
-            foreach ($newBorns as $fish) {
-                $this->simulationPool[$index++] = $fish;
-            }
-        }
-        $count = count($this->simulationPool);
-        $this->simulationPool = new SplFixedArray(0);
-        return $count;
-    }
-}
-
-class Fish
-{
-    public function __construct(private int $age) { }
-
-    public function getAge(): int
-    {
-        return $this->age;
-    }
-
-    public function doAge(): Fish|null
-    {
-        if ($this->age === 0) {
-            $this->age = 6;
-            return new Fish(8);
-        }
-        $this->age = $this->age - 1;
-        return null;
     }
 }
